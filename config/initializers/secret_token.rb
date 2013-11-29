7ca477ca0239598a6f78d421e00b7aefa2ba87f5
Seo Seokjin <usemodj@gmail.com>
2013-11-30 00:52:51 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WikiCloth::Application.config.secret_key_base = 'c5ba50bcce3d527bd09f8950cdc29720739c4633385be01eb2f667d422d330c185d9f838141f6bf7c58841c44891f4f09d34a8a92ed58a736054f6addb07f400'
